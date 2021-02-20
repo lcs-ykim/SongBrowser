7313e1eadb3aed271030a4552a818467f0b5063e
Yeseo Kim <YKim@lcs.on.ca>
2021-02-20 09:50:02 -0500

    Initial Commit

---
+++ SongBrowser/SongBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  SongBrowserApp.swift
+//  SongBrowser
+//
+//  Created by Yeseo Kim on 2021-02-20.
+//
+
+import SwiftUI
+
+@main
+struct SongBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
