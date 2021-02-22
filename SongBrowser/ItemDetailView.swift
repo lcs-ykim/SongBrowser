d6f42d1f5fd22669536676027c7b8002be58cffd
Yeseo Kim <YKim@lcs.on.ca>
2021-02-22 11:37:20 -0500

    Added four views that we will use to compose together a song detail view

---
+++ SongBrowser/ItemDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  ItemDetailView.swift
+//  SongBrowser
+//
+//  Created by Yeseo Kim on 2021-02-22.
+//
+
+import SwiftUI
+
+struct ItemDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ItemDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        ItemDetailView()
+    }
+}
---
+++ SongBrowser/SongDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  SongDetailView.swift
+//  SongBrowser
+//
+//  Created by Yeseo Kim on 2021-02-22.
+//
+
+import SwiftUI
+
+struct SongDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SongDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        SongDetailView()
+    }
+}
