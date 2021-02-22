96f10e1166e4a63c7b65d3593f2fb481560f87e3
Yeseo Kim <YKim@lcs.on.ca>
2021-02-22 11:28:01 -0500

    Extracted list item into its own view - could be re-used in future projects

--- SongBrowser/ContentView.swift
+++ SongBrowser/ContentView.swift
@@ -45,14 +45,9 @@ struct ContentView: View {
                 // to uniquely identify each song
                 List(songs, id: \.trackId) { currentSong in
                     
-                    VStack(alignment: .leading) {
-                        
-                        Text(currentSong.trackName)
-                        
-                        Text(currentSong.artistName)
-                            .font(.caption)
-                        
-                    }
+                    SimpleListItemView(title: currentSong.trackName,
+                                       caption: currentSong.artistName)
+                    
                 }
                 
             }
@@ -63,67 +58,33 @@ struct ContentView: View {
     // MARK: Functions
     func fetchSongResults() {
         
-        // Set the address of the JSON endpoint
-        let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song")!
+        let input = searchText.lowercased().replacingOccurrences(of: " ", with: "+")
+        
+        let url = URL(string: "https://itunes.apple.com/search?term=\(input)&entity=song")!
 
-        // Configure a URLRequest instance
-        // Defines what type of request will be sent to the address noted above
         var request = URLRequest(url: url)
         request.setValue("application/json",
                          forHTTPHeaderField: "Accept")
         request.httpMethod = "GET"
 
-        // Run the request on a background thread and process the result.
-        // NOTE: This occurs asynchronously.
-        //       That means we don't know precisely when the request will
-        //       complete.
         URLSession.shared.dataTask(with: request) { data, response, error in
 
-            // When the request *does* complete, there are three parameters
-            // containing data that are created:
-            //
-            // data
-            // The data returned by the server.
-            //
-            // response
-            // An object that provides response metadata, such as HTTP headers and status code. If you are making an HTTP or HTTPS request, the returned object is actually an HTTPURLResponse object.
-            //
-            // error
-            // An error object that indicates why the request failed, or nil if the request was successful.
-
-
-            // Verify that some data was actually returned
             guard let songData = data else {
 
-                // When no data is returned, provide a descriptive error
-                //
-                // error?.localizedDescription is an example of "optional chaining"
-                // This means that if the error object is not nil, the
-                // localizedDescription property will be used
-                //
-                // ?? "Unknown error" is an example of the "nil coalescing" operator
-                // This means that when the error object *is* nil, a default string of
-                // "Unknown error" will be provided
                 print("No data in response: \(error?.localizedDescription ?? "Unknown error")")
 
-                // Don't continue past this point
                 return
 
             }
 
-            // DEBUG: See what raw JSON data was returned from the server
               print(String(data: songData, encoding: .utf8)!)
 
-            // Attempt to decode the JSON into an instance of the SearchResult structure
             if let decodedSongData = try? JSONDecoder().decode(SearchResult.self, from: songData) {
 
-                // DEBUG:
                 print("Song data decoded from JSON successfully")
 
-                // Now, update the UI on the main thread
                 DispatchQueue.main.async {
 
-                    // Assign the result to the "songs" property
                     songs = decodedSongData.results
 
                 }
@@ -135,9 +96,6 @@ struct ContentView: View {
             }
 
         }.resume()
-        // NOTE: Invoking the resume() function
-        // on the dataTask closure is key. The request will not
-        // run, otherwise.
 
     }
 }
